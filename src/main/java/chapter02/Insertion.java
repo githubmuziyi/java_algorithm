c01b38e57353e23f85eac27f28d478c94fb42f24
muzi <Hw19921203>
2018-01-10 14:58:00 +0800

    去除多余标点符号

--- src/main/java/chapter02/Insertion.java
+++ src/main/java/chapter02/Insertion.java
@@ -23,7 +23,7 @@ package chapter02;
  */
 public class Insertion {
 
-    public static void sort(Comparable[] a) {;
+    public static void sort(Comparable[] a) {
         int N = a.length;
         for (int i = 1; i < N; i++) {
             for (int j = i; j > 0; j--) {
